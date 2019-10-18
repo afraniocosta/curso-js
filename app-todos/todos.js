1e3cefe394c855e6ad991fabb4ead132be2543cb
afraniocosta <afraniompc@gmail>
2019-10-17 21:45:52 -0300

    criação do app todo

--- app-todos/todos.js
+++ app-todos/todos.js
@@ -2,8 +2,46 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var todos = {
+var todos = [
     'Fazer café',
     'Estudar Javascript',
     'Acessar comunidade Rockseat'
-};
+];
+
+function renderTodos(){
+
+    listElement.innerHTML = '';
+
+    for (todo of todos) {
+
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+
+        var linkElement = document.createElement('a');
+
+        linkElement.setAttribute('href', '#')
+
+        var linkText = document.createTextNode('Excluir');
+
+        linkElement.appendChild(linkText);
+
+        todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
+
+        listElement.appendChild(todoElement)
+
+    }
+}
+
+renderTodos();
+
+function addTodo(){
+   
+    var todoText = inputElement.value;
+    todos.push(todoText);
+    inputElement.value = '';
+    renderTodos();
+
+}
+
+buttonElement.onclick = addTodo;
