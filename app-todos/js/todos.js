4d76ea891a8b7115e83732c213d27f0b0926b8ad
afraniocosta <afraniompc@gmail>
2019-10-21 22:16:51 -0300

    usando ajax

--- app-todos/js/todos.js
+++ app-todos/js/todos.js
@@ -2,11 +2,7 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var todos = [
-    'Fazer café',
-    'Estudar Javascript',
-    'Acessar comunidade Rockseat'
-];
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
 
 function renderTodos(){
 
@@ -20,15 +16,16 @@ function renderTodos(){
         var linkElement = document.createElement('a');
 
         linkElement.setAttribute('href', '#')
-
+        
+        var pos = todos.indexOf(todo);
+        linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')')
         var linkText = document.createTextNode('Excluir');
-
         linkElement.appendChild(linkText);
 
         todoElement.appendChild(todoText);
         todoElement.appendChild(linkElement);
 
-        listElement.appendChild(todoElement)
+        listElement.appendChild(todoElement);
 
     }
 }
@@ -41,7 +38,18 @@ function addTodo(){
     todos.push(todoText);
     inputElement.value = '';
     renderTodos();
+    saveToStorage();
 
 }
 
 buttonElement.onclick = addTodo;
+
+function deleteTodo(pos) {
+    todos.splice(pos, 1)
+    renderTodos();
+    saveToStorage();
+}
+
+function saveToStorage(){
+    localStorage.setItem('list_todos', JSON.stringify(todos))
+}
